036da4da1689f7775aa6eb6f50c1abcbb406658f
Wester <wester@choiceform.com>
2025-05-07 00:19:55 +0800

    Add initial setup for Angular, Next.js, Solid, and Vue examples
    - Created example projects for Angular, Next.js, Solid, and Vue, including basic configurations and structure.
    - Added essential files such as package.json, README.md, and configuration files for each framework.
    - Implemented basic components and styles to demonstrate usage of @choiceform/icons in each example.
    - Included .gitignore files to exclude unnecessary files from version control.
    - Set up initial routing and application structure for Angular and Next.js examples.

---
+++ examples/solid/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { Workspace } from '@choiceform/icons-solid';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <Workspace />
+    </div>
+  );
+}
+
+export default App;
